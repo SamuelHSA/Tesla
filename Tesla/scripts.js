2eb03244f580797fe7319dec50670569eda2c05e
SamuelHSA <111927634+SamuelHSA@users.noreply.github.com>
2024-07-17 13:42:22 -0300

    Add files via upload

---
+++ Tesla/scripts.js
@@ -0,0 +1,25 @@
+//document = ir ao HTML
+//querySelector = busca o que eu quiser que ele busque
+
+//Comprar
+let formulario = document.querySelector(".formulario")
+
+function aparecerFormulario() {
+    formulario.style.left = "30%"
+}
+
+function desaparecerFormulario() {
+    formulario.style.left = "-50%"
+}
+
+
+//Test-Drive
+let formularioTestDrive = document.querySelector(".formularioTestDrive")
+
+function aparecerFormularioTestDrive() {
+    formularioTestDrive.style.left = "30%"
+}
+
+function desaparecerFormularioTestDrive() {
+    formularioTestDrive.style.left = "-50%"
+}
